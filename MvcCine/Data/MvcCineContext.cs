d4f9472bbb640ec6c2c32732946a07d295c50d79
IkerMartin <ikermartinprog@gmail.com>
2022-05-04 12:03:08 +0200

    Primera versión

---
+++ MvcCine/Data/MvcCineContext.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcCine.Models;
+
+namespace MvcCine.Data
+{
+    public class MvcCineContext : DbContext
+    {
+        public MvcCineContext (DbContextOptions<MvcCineContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcCine.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ MvcCine/Models/MovieGenreViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace MvcCine.Models
+{
+    public class MovieGenreViewModel
+    {
+        // Lista de películas
+        public List<Movie>? Movies { get; set; }
+        
+        // Contenedor de la lista de géneros
+        public SelectList? Genres { get; set; }
+
+        // Género seleccionado
+        public string? MovieGenre { get; set; }
+
+        // Texto introducido en el cuadro de búsqueda
+        public string? SearchString { get; set; }
+    }
+}
